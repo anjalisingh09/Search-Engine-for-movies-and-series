154ea443c0ea4c7b3cad94849b72575d5889dac3
anjalisingh09 <38291012+anjalisingh09@users.noreply.github.com>
2018-12-26 01:29:11 -0800

    Update movieMain.js

--- movieMain.js
+++ movieMain.js
@@ -16,18 +16,6 @@ myApp.config(function($routeProvider) {
 });
 
 myApp.controller('movieController', function ($scope, $http, $rootScope) {
-		// $scope.formData = {};
-		// $scope.message = "hero";
-		// $scope.logId = function($scope) {
-		// 	$scope.message = $scope.id;
-		// 	$scope.$apply();
-		// 	// console.log($scope.message);
-		// }
-		// // console.log($scope.message);
-		// // console.log($scope.formData.searchText.$modelValue)
-  //   	$http.get('http://www.omdbapi.com/?t='+ $scope.message +'&apikey=51f2bbd8&r=json').success(function (response) {
-  //       $scope.movie = response;
-  //   });
   		$scope.name = 'zero';
   		$scope.check = function(name){
   			$http({
