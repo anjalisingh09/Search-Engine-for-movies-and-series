b83549c100e0ab61e3abc0d3c434a8a7d65e62de
anjalisingh09 <38291012+anjalisingh09@users.noreply.github.com>
2018-12-26 01:03:34 -0800

    Update movieMain.js

--- movieMain.js
+++ movieMain.js
@@ -15,18 +15,31 @@ myApp.config(function($routeProvider) {
 		});
 });
 
-	myApp.controller('movieController', function ($scope, $http) {
+myApp.controller('movieController', function ($scope, $http) {
 		// $scope.formData = {};
-		$scope.message = "hero";
-		$scope.logId = function($scope) {
-			$scope.message = $scope.id;
-			$scope.$apply();
-			// console.log($scope.message);
-		}
-		// console.log($scope.message);
-		// console.log($scope.formData.searchText.$modelValue)
-    	$http.get('http://www.omdbapi.com/?t='+ $scope.message +'&apikey=51f2bbd8&r=json').success(function (response) {
-        $scope.movie = response;
-    });
+		// $scope.message = "hero";
+		// $scope.logId = function($scope) {
+		// 	$scope.message = $scope.id;
+		// 	$scope.$apply();
+		// 	// console.log($scope.message);
+		// }
+		// // console.log($scope.message);
+		// // console.log($scope.formData.searchText.$modelValue)
+  //   	$http.get('http://www.omdbapi.com/?t='+ $scope.message +'&apikey=51f2bbd8&r=json').success(function (response) {
+  //       $scope.movie = response;
+  //   });
+  		$scope.name = 'zero';
+  		$scope.check = function(name){
+  			$http({
+  				method: "GET",
+  				url: "http://www.omdbapi.com/?t="+name+"&apikey=51f2bbd8&r=json"
+  			}).then(function mySuccess(resp){
+  				console.log("success")
+  				console.log(resp.data)
+  				$scope.movie = resp.data;
+  			},function myError(response){
+  				console.log("error");
+  			});
+  		}
 
 })
